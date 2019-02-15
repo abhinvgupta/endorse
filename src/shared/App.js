df65d5b8d950892eb44535c3f64b5022ecaba2f2
abhinav gupta <ruffneck@abhinavs-MacBook-Pro.local>
2019-02-15 12:15:14 +0530

    lint

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 import Counter from './Counter'
 
-let color = 'green';
+let color = 'black';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    // the following bindings are necessary to make `this` work in the callback
-  
-
-    console.log(' printable', props.style, typeof props.style)
     color = props.style.counterColor
   }
   render() {
